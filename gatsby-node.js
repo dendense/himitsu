61f91178c8c058814f071e8919f3611656f88ad9
Pillamore <jgeekudin@gmail.com>
2020-08-27 22:49:32 +0700

    Add Tags Page

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,6 +3,7 @@ const path = require("path")
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
   const postTemplate = path.resolve("src/templates/blogTemplate.js")
+  const tagsTemplate = path.resolve("src/templates/tagsTemplate.js")
 
   return graphql(`
     {
@@ -11,6 +12,7 @@ exports.createPages = ({ actions, graphql }) => {
           node {
             frontmatter {
               path
+              tags
             }
           }
         }
@@ -20,10 +22,22 @@ exports.createPages = ({ actions, graphql }) => {
     if (res.errors) {
       return Promise.reject(res.errors)
     }
+    var tags = [];
     res.data.allMarkdownRemark.edges.forEach(({ node }) => {
       createPage({
         path: node.frontmatter.path,
         component: postTemplate,
+      })
+      for (let tag of node.frontmatter.tags) tags.push(tag);
+    })
+    tags = Array.from(new Set(tags));
+    tags.forEach(tag => {
+      createPage({
+        path: `/tag/${tag.toLowerCase()}`,
+        component: tagsTemplate,
+        context: {
+          tags: tag
+        }
       })
     })
   })
