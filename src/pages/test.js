360d7ef52c64ed725c0f5500d84da30f15a540e3
DenDense <denyfarras@gmail.com>
2020-08-28 20:02:03 +0700

    Minor Update

--- src/pages/test.js
+++ src/pages/test.js
@@ -4,11 +4,34 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 import PostSmall from "../components/PostSmall"
+import "../styles/index.scss"
 
 const TestPage = ({ data }) => {
   return (
     <Layout>
       <SEO title="Testing Page" />
+      <div className="row post-small">
+        <div className="col-md">
+          <ul className="list-group">
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+          </ul>
+        </div>
+        <div className="col-md">
+          <ul className="list-group">
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+          </ul>
+        </div>
+      </div>
       <div className="mt-3">
         <div className="row row-cols-1 row-cols-md-3">
           {data.allMarkdownRemark.edges.map(post => {
--- src/templates/tagsTemplate.js
+++ src/templates/tagsTemplate.js
@@ -1,67 +1,67 @@
-import React from 'react';
-import { graphql } from 'gatsby';
+import React from "react"
+import { graphql } from "gatsby"
 
-import Layout from '../components/Layout';
+import Layout from "../components/Layout"
 import Post from "../components/Post"
-import SEO from '../components/Seo';
+import SEO from "../components/Seo"
 
 export default function Template({ data, pageContext }) {
-    const { tags } = pageContext;
-    return (
-        <Layout>
-            <SEO title={'Tags ' + tags}/>
-            <div className="mt-3">
-                <h1>Post with tags {tags}</h1>
-                <div className="row row-cols-1 row-cols-md-3">
-                    {data.allMarkdownRemark.nodes.map(post => {
-                        const {
-                            title,
-                            author,
-                            date,
-                            description,
-                            path,
-                            tags
-                        } = post.frontmatter;
-                        const image = post.frontmatter.image.childImageSharp.fluid.src;
-                        return (
-                            <Post
-                                key={`${date}__${title}`}
-                                title={title}
-                                author={author}
-                                date={date}
-                                description={description}
-                                path={path}
-                                image={image}
-                                tags={tags}
-                            ></Post>
-                        )
-                    })}
-                </div>
-            </div>
-        </Layout>
-    )
+  const { tags } = pageContext
+  return (
+    <Layout>
+      <SEO title={"Tags " + tags} />
+      <div className="mt-3">
+        <h1>Post with tags {tags}</h1>
+        <div className="row row-cols-1 row-cols-md-3">
+          {data.allMarkdownRemark.nodes.map(post => {
+            const {
+              title,
+              author,
+              date,
+              description,
+              path,
+              tags,
+            } = post.frontmatter
+            const image = post.frontmatter.image.childImageSharp.fluid.src
+            return (
+              <Post
+                key={`${date}__${title}`}
+                title={title}
+                author={author}
+                date={date}
+                description={description}
+                path={path}
+                image={image}
+                tags={tags}
+              ></Post>
+            )
+          })}
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export const tagsQuery = graphql`
-    query TagsPosts($tags: String!) {
-        allMarkdownRemark(filter: {frontmatter: {tags: {eq: $tags}}}) {
-            nodes {
-                frontmatter {
-                    date(formatString: "DD MMMM, YYYY")
-                    title
-                    description
-                    author
-                    path
-                    tags
-                    image {
-                        childImageSharp {
-                            fluid(maxWidth: 500) {
-                                src
-                            }
-                        }
-                    }
-                }
+  query TagsPosts($tags: String!) {
+    allMarkdownRemark(filter: { frontmatter: { tags: { eq: $tags } } }) {
+      nodes {
+        frontmatter {
+          date(formatString: "DD MMMM, YYYY")
+          title
+          description
+          author
+          path
+          tags
+          image {
+            childImageSharp {
+              fluid(maxWidth: 500) {
+                src
+              }
             }
+          }
         }
+      }
     }
+  }
 `
