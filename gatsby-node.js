899b2c19cc9348030844f87dd3a973f1e39499fb
Pillamore <jgeekudin@gmail.com>
2020-09-02 21:58:09 +0700

    Add Index Page Pagination and Fix Fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,7 @@ const ouo = new OuoPromise("zPdQ4s7s")
 
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
+  const indexTemplate = path.resolve("src/templates/indexTemplate.js")
   const postTemplate = path.resolve("src/templates/blogTemplate.js")
   const tagsTemplate = path.resolve("src/templates/tagsTemplate.js")
   const groupTemplate = path.resolve("src/templates/groupTemplate.js")
@@ -35,6 +36,24 @@ exports.createPages = ({ actions, graphql }) => {
     if (res.errors) {
       return Promise.reject(res.errors)
     }
+    // index containing blog list with pagination
+    const totalItems = res.data.allMarkdownRemark.edges.length;
+    const itemsPerPage = 8;
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    console.log(totalItems, itemsPerPage, totalPages);
+    Array.from({ length: totalPages }).forEach((val, i) => {
+      createPage({
+        path: i===0 ? '/' : `/index/${i+1}`,
+        component: indexTemplate,
+        context: {
+          limit: itemsPerPage,
+          skip: i * itemsPerPage,
+          totalPages: totalPages,
+          currentPage: i+1
+        }
+      })
+    })
+    // blog post
     res.data.allMarkdownRemark.edges.forEach(async ({ node }) => {
       let shortenedLink = await ouo.shortMany(
         node.frontmatter.link.map(i => i.url)
@@ -47,6 +66,7 @@ exports.createPages = ({ actions, graphql }) => {
         },
       })
     })
+    // tag page (music, musician, nogi, concert, dll)
     res.data.allMarkdownRemark.distinct.forEach(tag => {
       createPage({
         path: `/tag/${tag.toLowerCase()}`,
@@ -56,6 +76,7 @@ exports.createPages = ({ actions, graphql }) => {
         },
       })
     })
+    // group page (sakamichi, 48g, chika, dll)
     groupPagesConfig.forEach(group => {
       console.log(group)
       createPage({
