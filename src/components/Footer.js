7ae512aae1b345ac0b487f795ab1f74d5e7cd320
Pillamore <jgeekudin@gmail.com>
2020-09-05 15:56:02 +0700

    Fix Footer Component Classname

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,12 @@ import "../styles/index.scss"
 export default function Footer(props) {
   return (
     <div style={{ position: 'absolute', 'bottom': 0, width: '100%' }}>
-      <div class="container-fluid himitsu-footer">
+      <div className="container-fluid himitsu-footer">
         <div className="row p-3">
-          <div class="col-sm p-1">
+          <div className="col-sm p-1">
             <IconText TextIcon="H">HIMITSU MEDIA</IconText>
           </div>
-          <div class="col-auto p-1">
+          <div className="col-auto p-1">
             <IconText TextIcon="💖">Made With Love</IconText>
           </div>
         </div>
